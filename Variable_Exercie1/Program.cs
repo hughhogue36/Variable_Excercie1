724b72499c10717d5a8743a43984a9ec00b38263
hughhogue36 <hvh18236@gmail.com>
2023-01-15 16:13:06 -0600

    initial commit

---
+++ Variable_Exercie1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string catName = "Aerith";
+int catAge = 3;
+char catMiddleInitial = 'S';
+bool isAlwaysHungry = true;
+double currentWeight = 7.62;
+decimal currentHeight = 10.5m;
+
+Console.WriteLine($"My cat's name is {catName}, She is {catAge} year's old.");
+Console.WriteLine($"Aerith's middle initial {catMiddleInitial}, she is {currentHeight} inches tall, and weighs {currentWeight}lbs.");
+Console.WriteLine($"It is {isAlwaysHungry} she is always hungry but she is still a small cat.");
